a823bcc4e3bdd83869b5263df02a512a27a9f382
shravan6006 <shravan6006@gmail.com>
2021-12-04 20:08:04 +0530

    Controller class added

---
+++ src/main/java/com/app/programming/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.app.programming.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping("/greet")
+	public String greet() {
+		String msg="Hello EveryOne Good Evening";
+		return msg;
+	}
+
+}
