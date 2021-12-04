e2e0def050a04d5ed4f6f85a25171ddb2d7d8e7a
shravan6006 <shravan6006@gmail.com>
2021-12-04 13:46:39 +0530

    added sop statement

--- src/main/java/com/app/programming/SpringWebMvcApplication.java
+++ src/main/java/com/app/programming/SpringWebMvcApplication.java
@@ -8,6 +8,7 @@ public class SpringWebMvcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebMvcApplication.class, args);
-	}
-
+	
+System.out.println("This is the main class");
+}
 }
